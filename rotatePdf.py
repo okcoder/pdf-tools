d42fd12c5c5f95c52b4208d2dcafc066cd67447a
cboittin <cboittin.public+github@gmail.com>
2015-09-14 17:06:35 +0200

    Update rotatePdf.py

--- rotatePdf.py
+++ rotatePdf.py
@@ -8,19 +8,32 @@ parser.add_argument("input", help="Input pdf file")
 parser.add_argument("output", help="Output pdf file")
 parser.add_argument("-angle", default=90, help="Rotation angle")
 parser.add_argument("-direction", default="counterclockwise", help="One of 'clockwise' or 'counterclockwise'")
+parser.add_argument("-pages", default="all", help="Python list (with enclosing [] and comma separated) of page numbers, starting from 1.\n\tExample: [1, 7, 8, 13]")
 
 args = parser.parse_args()
 angle = args.angle
 direction = args.direction
+PAGES = False
+if args.pages != "all":
+    PAGES = True
+    pages = sorted(eval(args.pages))
 
 from pyPdf import PdfFileWriter, PdfFileReader
 inFile = PdfFileReader(file(args.input, "rb"))
 outFile = PdfFileWriter()
+
+if PAGES:
+    nextPage = pages.pop(0)
+
 for i in range(0,inFile.getNumPages()):
     page = inFile.getPage(i)
-    if direction == "counterclockwise":
-        outFile.addPage(page.rotateCounterClockwise(args.angle))
+    if PAGES and (nextPage is None or i+1 != nextPage):
+        outFile.addPage(page)
     else:
-        outFile.addPage(page.rotateClockwise(args.angle))
+        if direction == "counterclockwise":
+            outFile.addPage(page.rotateCounterClockwise(args.angle))
+        else:
+            outFile.addPage(page.rotateClockwise(args.angle))
+        if PAGES and len(pages) > 0:
+            nextPage = pages.pop(0)
     outFile.write(file(args.output, "wb"))
-
